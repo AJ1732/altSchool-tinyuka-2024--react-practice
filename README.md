5297b20d2febcc0ef1e53e06bb91eac63c2c6d55
AJ1732 <ejemeniboi@gmail.com>
2025-03-21 05:30:19 +0100

    init: setup react project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# altSchool-tinyuka-2024--react-practice
-Repository for AltSchool Tinyuka Class of 2024 React practice
+# React Practice
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="content-grid bg-background text-foreground min-h-dvh">
+      <section>Hello World</section>
+    </div>
+  );
+}
+
+export default App;
